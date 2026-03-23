708981f2f5e3b4c22e21f38d78a4954fed554699
jackddouglas <jackddouglas@users.noreply.github.com>
2026-03-23 15:24:42 +0000

    deploy: tonk-labs/tonk@83a2a0b64028fdf8d85f66bfddfd6c4964011039

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1a058eae.js',
+                path_to_root + 'searchindex-f3261ebe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
